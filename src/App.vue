9743b567e942c975a248b87c2fccba1aa7f0bb61
shaunhk <shaunhk@hotmail.com>
2019-06-21 20:29:27 +0100

    present

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
-<template>
+<template lang="html">
   <div id="app">
+    <p id="list" v-for="companyProfile in this.companyProfiles"><img id= "logo" :src="companyProfile.image">Company name: {{companyProfile.companyName}} Stock-price: {{companyProfile.price}}</p>
   </div>
 </template>
 
@@ -9,7 +10,27 @@ export default {
   name: 'app',
   components: {
 
+  },
+  data(){
+    return {
+      companyProfiles: null,
+      companySymbols: ['MSFT', 'APPL']
+    }
+  },
+  mounted() {
+    this.getCompanyProfile
+  },
+  methods: {
+    getCompanyProfile(){
+      const promises = companySymbols.map(symbol => {
+    return fetch(`https://financialmodelingprep.com/api/v3/company/profile/${symbol}`)
+    .then(response => response.json())
+  })
+
+  Promise.all(promises)
+  .then(data => this.companyProfiles = data.map(company => {company.profile}))
   }
+}
 }
 </script>
 
@@ -22,4 +43,14 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+#logo {
+  width: 20px;
+  height: 20px;
+}
+
+#list {
+  text-align: left;
+}
+
 </style>
